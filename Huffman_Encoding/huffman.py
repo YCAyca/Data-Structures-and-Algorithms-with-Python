732103b49ca26f5e636170e2167fa4cf030f5b3d
YCAyca <aktas.yagmur@gmail.com>
2021-08-12 20:23:34 +0200

    Update huffman.py
    decoding added

--- Huffman_Encoding/huffman.py
+++ Huffman_Encoding/huffman.py
@@ -43,11 +43,26 @@ def Calculate_Probability(data):
             symbols[element] += 1     
     return symbols
 
-
-""" A helper function to print the encoded data"""
-def Print_Encoded(data, coding):
+""" A helper function to obtain the encoded output"""
+def Output_Encoded(data, coding):
+    encoding_output = []
     for c in data:
-        print(coding[c], end = '')
+      #  print(coding[c], end = '')
+        encoding_output.append(coding[c])
+        
+    string = ''.join([str(item) for item in encoding_output])    
+    return string
+        
+""" A helper function to calculate the space difference between compressed and non compressed data"""    
+def Total_Gain(data, coding):
+    before_compression = len(data) * 8 # total bit space to stor the data before compression
+    after_compression = 0
+    symbols = coding.keys()
+    for symbol in symbols:
+        count = data.count(symbol)
+        after_compression += count * len(coding[symbol]) #calculate how many bit is required for that symbol in total
+    print("Space usage before compression (in bits):", before_compression)    
+    print("Space usage after compression (in bits):",  after_compression)           
 
 def Huffman_Encoding(data):
     symbol_with_probs = Calculate_Probability(data)
@@ -65,12 +80,12 @@ def Huffman_Encoding(data):
     while len(nodes) > 1:
         # sort all the nodes in ascending order based on their probability
         nodes = sorted(nodes, key=lambda x: x.prob)
-        #for node in nodes:  
-         #    print(node.symbol, node.prob)
+        # for node in nodes:  
+        #      print(node.symbol, node.prob)
     
         # pick 2 smallest nodes
-        left = nodes[0]
-        right = nodes[1]
+        right = nodes[0]
+        left = nodes[1]
     
         left.code = 0
         right.code = 1
@@ -81,62 +96,47 @@ def Huffman_Encoding(data):
         nodes.remove(left)
         nodes.remove(right)
         nodes.append(newNode)
-    
+            
     huffman_encoding = Calculate_Codes(nodes[0])
-    print(huffman_encoding)
+    print("symbols with codes", huffman_encoding)
     Total_Gain(data, huffman_encoding)
-    print("Encoded :")
-    Print_Encoded(data,huffman_encoding)
+    encoded_output = Output_Encoded(data,huffman_encoding)
+    return encoded_output, nodes[0]  
     
-""" A helper function to calculate the space difference between compressed and non compressed data"""    
-def Total_Gain(data, coding):
-    before_compression = len(data) * 8 # total bit space to stor the data before compression
-    after_compression = 0
-    symbols = coding.keys()
-    for symbol in symbols:
-        count = data.count(symbol)
-        after_compression += count * len(coding[symbol]) #calculate how many bit is required for that symbol in total
-    print("Space usage before compression (in bits):", before_compression)    
-    print("Space usage after compression (in bits):",  after_compression)    
-
-
+ 
+def Huffman_Decoding(encoded_data, huffman_tree):
+    tree_head = huffman_tree
+    decoded_output = []
+    for x in encoded_data:
+        if x == '1':
+            huffman_tree = huffman_tree.right   
+        elif x == '0':
+            huffman_tree = huffman_tree.left
+        try:
+            if huffman_tree.left.symbol == None and huffman_tree.right.symbol == None:
+                pass
+        except AttributeError:
+            decoded_output.append(huffman_tree.symbol)
+            huffman_tree = tree_head
+        
+    string = ''.join([str(item) for item in decoded_output])
+    return string        
 
 
 """ First Test """
-data = "DBAEABCAAADEABCACBAAEABAACDBDAABCDE"
+data = "AAAAAAABCCCCCCDDEEEEE"
 print(data)
-Huffman_Encoding(data)
-
-# """ Second Test """
-
-data = """Einstein was born in the German Empire, 
-but moved to Switzerland in 1895, forsaking his German citizenship 
-(as a subject of the Kingdom of Württemberg)[note 1] the following year. 
-In 1897, at the age of 17, he enrolled in the mathematics and physics 
-teaching diploma program at the Swiss Federal polytechnic school 
-(later renamed as ETH Zurich) in Zürich, graduating in 1900. 
-In 1901 he acquired Swiss citizenship, which he kept for the rest of his 
-life, and in 1903 he secured a permanent position at the Swiss Patent Office in Bern. 
-In 1905, he was awarded a PhD by the University of Zurich. In 1914, 
-Einstein moved to Berlin in order to join the Prussian Academy of Sciences and the Humboldt 
-University of Berlin. In 1917, Einstein became director of the Kaiser Wilhelm Institute for 
-Physics; he also became a German citizen again – Prussian this time. In 1933, while 
-Einstein was visiting the United States, Adolf Hitler came to power. Einstein did not return 
-to Germany because he objected to the policies of the newly elected Nazi-led government.[16] 
-He settled in the United States and became an American citizen in 1940.[17] On the eve of 
-World War II, he endorsed a letter to President Franklin D. Roosevelt alerting him to the potential 
-German nuclear weapons program and recommending that the US begin similar research. 
-Einstein supported the Allies, but generally denounced the idea of nuclear weapons."""
-print(data)
-Huffman_Encoding(data)
-
-""" Third Test """
+encoding, tree = Huffman_Encoding(data)
+print("Encoded output", encoding)
+print("Decoded Output", Huffman_Decoding(encoding,tree))
 
 
-f = open("demofile.txt", "r")
+""" Second Test """
 
-data = f.read()
-print(data)
-Huffman_Encoding(data)
+# f = open("demofile.txt", "r")
+
+# data = f.read()
+# print(data)
+# Huffman_Encoding(data)
 
 
